28567f7fb1afd58b0bea7fab0a24cb786bcac38b
Florian Hübner <code@hub.lol>
2024-02-25 19:37:25 +0100

    scan network

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,3 +17,17 @@ tofu apply \
   -no-color \
   -auto-approve \
   -var="ssh_key=$(cat ~/.ssh/id_ed25519.pub)"
+
+# wait for ssh
+while ! ssh \
+    -o BatchMode=yes \
+    -o ConnectTimeout=1 \
+    -o StrictHostKeyChecking=no \
+    "root@10.42.0.10" \
+    true; do
+    sleep 1
+done
+
+# scan network
+sleep 1
+nmap -n -sn -oG - 10.42.0.0/24 | awk '/Up$/{print $2}' | tail -n +2
