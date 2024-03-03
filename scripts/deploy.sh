9e2a1b4e307f2ff297312b8fd4f7d47fa1d7da35
Florian Hübner <fhuebner@posteo.de>
2024-03-03 18:33:43 +0100

    kubespray

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -58,12 +58,35 @@ info 'Purging old infra leftovers...'
 # deploy cluster
 {
     if test ! -d kubespray; then
-        git clone --branch 'v2.24.1' --single-branch \
+        info 'Fetching Kubespray...'
+        git clone --branch 'release-2.24' --single-branch \
             https://github.com/kubernetes-sigs/kubespray.git
         rm -rf kubespray/.git
     fi
 
-    # TODO
+    (
+        cd kubespray
+
+        # install requirements in venv
+        python3 -m venv venv
+        source venv/bin/activate
+        pip install -r requirements.txt
+
+        # generate inventory
+        cp -rfp inventory/sample inventory/mycluster
+        # shellcheck disable=SC2046
+        CONFIG_FILE=inventory/mycluster/hosts.yml \
+            python3 contrib/inventory_builder/inventory.py \
+            $(yq -r '.hosts | to_entries | .[].value[].ipv4' config.yaml)
+
+        info 'Deploying cluster...'
+        ansible-playbook \
+            --inventory-file inventory/mycluster/hosts.yml \
+            -u 'janitor' \
+            --verbose \
+            --become \
+            cluster.yml
+    )
 }
 
 info '🏁 Done!'
