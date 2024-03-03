2b8f5dd9ef8d5c93f1303d55ff10ed69882811ef
Florian Hübner <fhuebner@posteo.de>
2024-03-03 14:57:40 +0100

    better ssh await

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-set -eu
+set -o errexit
+set -o nounset
+set -o pipefail
 
 create_machines() {
     tofu init \
@@ -13,6 +15,8 @@ create_machines() {
         -no-color \
         -auto-approve \
         -var="ssh_key=$(cat 'secrets/ssh.yaml' | yq -r '.pub_keys[0]')"
+
+    await_ssh "$(cat inventory.yaml | yq -r '.ctrl.hosts | to_entries | .[0].value.ansible_host')"
 }
 
 deploy_machines() {
@@ -20,16 +24,17 @@ deploy_machines() {
 }
 
 await_ssh() {
-    host="$(cat inventory.yaml | yq -r '.ctrl.hosts.ctrl0.ansible_host')"
-    # wait for ssh
     while ! ssh \
         -o BatchMode=yes \
         -o ConnectTimeout=1 \
         -o StrictHostKeyChecking=no \
-        "janitor@${host}" \
-        true; do
+        "janitor@${1}" \
+        true 2>/dev/null; do
         sleep 1
+        printf '😴'
     done
+    sleep 1
+    printf '👌\n'
 }
 
 # workdir is repository root
@@ -39,4 +44,3 @@ set -x
 
 create_machines
 deploy_machines
-await_ssh
